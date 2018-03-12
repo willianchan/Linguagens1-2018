0362ae8a53d5b1f32d3c72637a7c38a980064bef
Chan <willian_chan_@hotmail.com>
2018-03-12 12:54:59 -0300

    exportando ex4

---
+++ pratica1/exercicio4/Aula03/src/aula03/Aula03.java
@@ -0,0 +1,11 @@
+package aula03;
+
+public class Aula03 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("teclado filho da puta do caralho");
+      
+    }
+    
+}
