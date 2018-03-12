ad1b16ea2fcb2cc609fed9aa27affc6dd1c31bce
Chan <willian_chan_@hotmail.com>
2018-03-12 12:39:07 -0300

    subindo exs 2 e 3

---
+++ pratica1/exercicio2/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma{
+	public static void main(String[] args){
+	
+		//Mensagem de saida
+		System.out.println("Minha primeira aplicação Java!!");
+	}
+}
---
+++ pratica1/exercicio3/MeuPrograma.java
@@ -0,0 +1,10 @@
+class MeuPrograma{
+	public static void main(String[] args){
+	
+		int i;
+		for( i = 0; i < args.length; i++){
+			System.out.println(args[i]);
+		}
+		
+	}
+}
